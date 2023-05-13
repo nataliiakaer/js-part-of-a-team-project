eae2bac78f4b83c2faf8b75ab81ae8864fd85300
nataliiakaer <112327330+nataliiakaer@users.noreply.github.com>
2023-05-13 22:22:34 +0000

    Deploying to gh-pages from @ nataliiakaer/js-part-of-a-team-project@5a98f209c8dd2cfe5d05e7e97d3d8ad473f4a8b6 🚀

--- index.34db8aec.js
+++
@@ -1,2 +0,0 @@
-const e=new URL("https://developer.mozilla.org/en-US/docs/Web/API/URL/search?q=123");console.log(e.search);
-//# sourceMappingURL=index.34db8aec.js.map
--- index.50ef2a9f.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=new URL("https://developer.mozilla.org/en-US/docs/Web/API/URL/search?q=123");console.log(e.search)}();
-//# sourceMappingURL=index.50ef2a9f.js.map
