7640cbae4f54efa59c4244a5cc4dbc68a2c68032
Nataliia Kruhliak <nataliia.kaer@gmail.com>
2023-05-15 14:42:41 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-import Swiper from 'swiper';
-//import Swiper, { Navigation } from 'swiper';
+import Swiper, { Navigation } from 'swiper';
 import 'swiper/swiper-bundle.min.css';
 import { supportList } from './js/support_list';
 import { createImageItemMurkup } from './js/createMarkupSupportSection';
-
-Swiper.use(Navigation);
 
 const supportContainer = document.querySelector('.swiper-wrapper');
 const imageMarkup = createImageItemMurkup(supportList);
@@ -13,9 +10,8 @@ supportContainer.insertAdjacentHTML('beforeend', imageMarkup);
 
 swiperContainer = document.querySelector('.swiper-container');
 
-// Swiper.use([Navigation]);
 swiperOptions = {
- // modules: [Navigation],
+  modules: [Navigation],
   direction: 'vertical',
   slidesPerView: 4,
   spaceBetween: 20,
