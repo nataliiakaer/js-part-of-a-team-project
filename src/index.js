d2a0d74ff0573db13ce348dab6f0887c67a49156
Nataliia Kruhliak <nataliia.kaer@gmail.com>
2023-05-15 15:30:13 +0300

    update

--- src/index.js
+++ src/index.js
@@ -8,9 +8,34 @@ const imageMarkup = createImageItemMurkup(supportList);
 
 supportContainer.insertAdjacentHTML('beforeend', imageMarkup);
 
-swiperContainer = document.querySelector('.swiper-container');
+// swiperContainer = document.querySelector('.swiper-container');
 
-swiperOptions = {
+// swiperOptions = {
+//   modules: [Navigation],
+//   direction: 'vertical',
+//   slidesPerView: 4,
+//   spaceBetween: 20,
+//   rewind: true,
+//   speed: 400,
+//   loop: true,
+//   paginationClickable: true,
+//   grabCursor: true,
+//   waitForTransition: false,
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+//   breakpoints: {
+//     1440: {
+//       slidesPerView: 6,
+//       spaceBetween: 20,
+//     },
+//   },
+// };
+
+// carousel = new Swiper(swiperContainer, swiperOptions);
+
+new Swiper('.swiper-container', {
   modules: [Navigation],
   direction: 'vertical',
   slidesPerView: 4,
@@ -31,6 +56,4 @@ swiperOptions = {
       spaceBetween: 20,
     },
   },
-};
-
-carousel = new Swiper(swiperContainer, swiperOptions);
+});
